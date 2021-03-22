f5ee112599d17763206b60a516bc0cf2f254ec45
Тимур Бочкарев <tmbochkarev@outlook.com>
2021-03-22 16:47:26 +0700

    Create program

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo culture = CultureInfo.CreateSpecificCulture("en-US");
+
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CultureInfo.DefaultThreadCurrentUICulture = culture;
+
+            Console.WriteLine("Input your number: ");
+
+            decimal rouble = Convert.ToDecimal(Console.ReadLine());
+            
+            
+            decimal dollar = rouble / (decimal)74.25;
+
+            decimal euro = rouble / (decimal)88.72;
+
+            dollar = Math.Round(dollar, 2);
+
+            euro = Math.Round(euro, 2);
+
+            Console.WriteLine($"Dollar is {dollar} for {rouble} roubles. Euro is {euro} for {rouble} roubles");
+
+        }
+    }
+}
---
+++ NewApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace NewApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input your number here: ");
+
+            long getNumber = Convert.ToInt64(Console.ReadLine());
+            
+            int getDigitsOfTheNumber = getNumber.ToString().Sum(c => c - '0');
+
+            int getReminderOfTheNumber = getDigitsOfTheNumber % 3;
+
+            
+
+            if ( getDigitsOfTheNumber % 3 == 0 )
+            {
+                Console.WriteLine($"Number { getNumber } divides on 3 without the reminder");
+            }
+            else
+            {
+                Console.WriteLine($"Number { getNumber } divides on 3 with the reminder");
+                Console.WriteLine($"Reminder of the number { getNumber } is { getReminderOfTheNumber }");
+            }
+        }
+    }
+}
