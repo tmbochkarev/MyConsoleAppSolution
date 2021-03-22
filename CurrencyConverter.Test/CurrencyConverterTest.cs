bb21b81f5730756bf4ffec99834436d184c18932
Evgenii Klimov <vikkes@yandex.ru>
2021-03-22 19:36:32 +0700

    add tests

---
+++ CurrencyConverter.Test/CurrencyConverterTest.cs
@@ -0,0 +1,43 @@
+﻿using MyConsoleApp.Services;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CurrencyConverter.Test
+{
+    public class CurrencyConverterTest
+    {
+
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public CurrencyConverterTest(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        [Theory]
+        [InlineData(100)]
+        [InlineData(220)]
+        [InlineData(10000)]
+        public void Convert_ToDollar_Test(decimal rubles)
+        {
+            var converter = new DollarConverter();
+            var result = converter.FromRubles(rubles);
+
+            _testOutputHelper.WriteLine($"result: {result}");
+            Assert.True(result != 0m);
+        }
+
+        [Theory]
+        [InlineData(100)]
+        [InlineData(220)]
+        [InlineData(10000)]
+        public void Convert_ToEuro_Test(decimal rubles)
+        {
+            var converter = new DollarConverter();
+            var result = converter.FromRubles(rubles);
+
+            _testOutputHelper.WriteLine($"result: {result}");
+            Assert.True(result != 0m);
+        }
+    }
+}
