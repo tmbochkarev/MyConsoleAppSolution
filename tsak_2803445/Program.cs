1578ae43ea1e2f094421dc3149a6eb03d9d280c2
Evgenii Klimov <vikkes@yandex.ru>
2021-03-26 19:29:16 +0700

    add task 2803445

---
+++ tsak_2803445/Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace tsak_2803445
+{
+	/// <summary>
+	/// Используя рекурсивную подпрограмму , найти сумму цифр заданного натурального числа
+	/// </summary>
+	/// <remarks>
+	/// https://www.cyberforum.ru/csharp-beginners/thread2803445.html
+	/// </remarks>
+	class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
